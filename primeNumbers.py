7068a5706248b8d0c01e0fc15ab6102ebe29f5ba
Joseph Chamish <jchamis1@binghamton.edu>
2018-07-14 10:38:53 -0400

    created check for prime numbers

---
+++ primeNumbers.py
@@ -0,0 +1,37 @@
+
+import random
+# prime numbers
+# A prime number has to be a positive integer
+# Divisible by exactly 2 integers (1 and itself)
+# 1 is not a prime number
+
+def isPrime(n):
+    bRet = True
+    if n < 2:
+        bRet = False
+
+    for i in range(2, n):
+            if n % i == 0:
+                bRet = False
+    return bRet
+
+
+def isPrimeFast(n):
+    bRet = True
+    if n < 2:
+        bRet = False
+
+    for i in range(2, int(n ** 0.5)+1, 2):
+        if n % i == 0:
+            bRet = False
+            break
+    return bRet
+
+
+print("{},{}".format(1, isPrime(1)))
+t = random.randint(0, 100)
+print("{},{}".format(t, isPrime(t)))
+print("{},{}".format(t, isPrimeFast(t)))
+t = random.randint(0, 100)
+print("{},{}".format(t, isPrime(t)))
+print("{},{}".format(t, isPrimeFast(t)))
