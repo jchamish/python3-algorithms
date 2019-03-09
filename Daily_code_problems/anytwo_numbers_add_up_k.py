0e5b086177f8b0c5b3f2c4a2f82761e795de1577
Joseph Chamish <jchamis1@binghamton.edu>
2019-03-09 13:43:35 -0500

    Adding daily_coding problems

---
+++ Daily_code_problems/anytwo_numbers_add_up_k.py
@@ -0,0 +1,36 @@
+# Good morning! Here's your coding interview problem for today.
+#
+# This problem was recently asked by Google.
+#
+# Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+#
+# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+#
+# Bonus: Can you do this in one pass?
+
+import random
+
+
+array_numbers = [10, 15, 3, 7]
+
+# N^2
+def sulotion_1(arr, k):
+    for idx, val in enumerate(arr):
+        for val_2 in arr[idx:]:
+            if val+val_2 == k:
+                return True
+    return False
+
+
+def sulotion_2(arr, k):
+    for idx, val in enumerate(arr):
+        remainder = k - val
+        if remainder in arr[idx:]:
+            return True
+    return False
+
+random_number = random.randint(1,21)
+
+print(random_number)
+print(sulotion_1(array_numbers, random_number))
+print(sulotion_2(array_numbers, random_number))
