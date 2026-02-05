42efa848bfd32fe7655d3c29e1e07ed8a4f80cde
JoeC <jcham55@gmail.com>
2026-02-05 17:00:45 -0500

    updating the LinkedList and removing API Test

--- LinkedList.py
+++ LinkedList.py
@@ -1,46 +1,44 @@
 import random
 
+from typing import Optional
 
-class Node(object):
-
-    def __init__(self, data=None):
+class Node:
+    def __init__(self, data: Optional[int]=None):
         self.data = data
-        self.next_node = None
+        self.next: Optional[Node] = None
+        self.prev: Optional[Node] = None
 
+class LinkedList:
 
-class LinkedList(object):
-    def __init__(self):
+    def __init__(self) -> None:
         self.size = 0
-        self.head = None
-        self.tail = None
+        self.head = Node(-1)
+        self.tail = Node(-1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
-    def insert(self, data):
-        tmp = Node(data)
-        tmp.next_node = None
-        if self.head is None:
-            self.head = tmp
-            self.tail = self.head
-            self.size += 1
-        else:
-            self.tail.next_node = tmp
-            self.tail = self.tail.next_node
-            self.size += 1
+    def insert(self, data: int) -> None:
+        node = Node(data)
+        last_elm = self.tail.prev
+        last_elm.next = node
+        node.next = self.tail
+        node.prev = last_elm
+        self.tail.prev = node
 
     # Find the first element that matches
-    def delete(self, data):
-        current_node = self.head
-        previous_node = None
-        while current_node is not None:
-            if current_node.data == data:
-                previous_node.next_node = current_node.next_node
-                if current_node is self.tail:
-                    self.tail = previous_node
-                return current_node
-            else:
-                previous_node = current_node
-                current_node = current_node.next_node
-        return None
-
+    # def delete(self, data):
+    #     current_node = self.head
+    #     previous_node = None
+    #     while current_node is not None:
+    #         if current_node.data == data:
+    #             previous_node.next = current_node.next
+    #             if current_node is self.tail:
+    #                 self.tail = previous_node
+    #             return current_node
+    #         else:
+    #             previous_node = current_node
+    #             current_node = current_node.next
+    #     return None
 
 # Start Main Program
 if __name__ == "__main__":
@@ -51,4 +49,4 @@ if __name__ == "__main__":
 
     linkedList.insert(5)
 
-    linkedList.delete(50)
+    # linkedList.delete(50)
